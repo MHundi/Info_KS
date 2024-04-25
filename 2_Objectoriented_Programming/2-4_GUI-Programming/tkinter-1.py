970dfc7455016ff2ce71f94525161a2e10383fa8
MHund <85964380+MHundi@users.noreply.github.com>
2024-04-25 20:18:38 +0200

    main

--- 2_Objectoriented_Programming/2-4_GUI-Programming/tkinter-1.py
+++ 2_Objectoriented_Programming/2-4_GUI-Programming/tkinter-1.py
@@ -3,9 +3,12 @@ import tkinter as tk
 root = tk.Tk()
 
 
-cv = tk.Canvas(root, height = 600, width = 400)
+cv = tk.Canvas(root, height = 600, width = 600)
 cv.pack()
 
 r = cv.create_rectangle(100,100,200,150, fill = "green")
 o = cv.create_oval(100,200,500,500, fill="red", width = 3)
+
+message = tk.Label(root, text =  o+r)
+message.pack( side = tk.BOTTOM )
 root.mainloop()
